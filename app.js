8e38ac133b2b85c28cc012f10fab65513e112723
Fernanda Santos <santos4fer@gmail.com>
2023-02-04 22:50:34 -0300

    testando console

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de Jogos")
 
 
 console.log("Aplicação está se conectando ao banco de dados")
+//mais um console de teste:
+console.log("A Fernanda esteve aqui")
